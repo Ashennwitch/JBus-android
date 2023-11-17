03ad0aa7fb6c0277cff5669b890fa0fce9fe968b
Ashennwitch <119783785+Ashennwitch@users.noreply.github.com>
2023-11-17 21:37:40 +0700

    CS08 - tes git

---
+++ app/src/main/java/com/HanifNurIlhamSanjayaJBusBR/LoginActivity.java
@@ -0,0 +1,44 @@
+package com.HanifNurIlhamSanjayaJBusBR;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
+import android.widget.Button;
+import android.widget.Toast;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private TextView registerNow = null;
+    private Button loginButton = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        getSupportActionBar().hide();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        registerNow = findViewById(R.id.donthaveanaccount_textview);
+        loginButton = findViewById(R.id.login_button);
+
+        registerNow.setOnClickListener(v -> {
+            moveActivity(this, RegisterActivity.class);
+        });
+
+        loginButton.setOnClickListener(v -> {
+            moveActivity(this, RegisterActivity.class);
+        });
+    }
+
+    private void moveActivity(Context ctx, Class <? > cls) {
+        Intent intent = new Intent(ctx, cls);
+        startActivity(intent);
+    }
+
+    private void viewToast(Context ctx, String message) {
+        Toast.makeText(ctx, message, Toast. LENGTH_SHORT) . show ();
+    }
+
+
+}
